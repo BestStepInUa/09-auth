ed8f988c599295a4a77209fe8198f63d364588a6
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-06-07 14:52:35 +0300

    fix bug with avatar src

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -12,7 +12,13 @@ export default function Profile() {
 					</a>
 				</div>
 				<div className={css.avatarWrapper}>
-					<Image src='Avatar' alt='User Avatar' width={120} height={120} className={css.avatar} />
+					<Image
+						src='/avatar.svg'
+						alt='User Avatar'
+						width={120}
+						height={120}
+						className={css.avatar}
+					/>
 				</div>
 				<div className={css.profileInfo}>
 					<p>Username: your_username</p>
@@ -22,4 +28,3 @@ export default function Profile() {
 		</main>
 	)
 }
-
